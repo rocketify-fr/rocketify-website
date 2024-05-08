8aa88933c5e084d12ea247e4374666ab245f7f0a
Louis Cathala <delwiv@protonmail.com>
2024-05-08 19:08:38 +0200

    feat: add more eslint config

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { Logo } from '~/components/Logo'
 import type { LogoProps } from '~/types/home'
 
-export function Footer(props: LogoProps) {
+export function Footer({data}) {
   return (
     <footer className='border-t border-gray-100 transition-colors duration-1000 ease-in-out dark:border-gray-900'>
       <div className='container mx-auto flex items-center justify-between p-4 lg:px-12'>
--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -5,8 +5,8 @@ export function Logo() {
     initial: {
       data: {
         header: { logo },
-      },
-    },
+      }
+    }
   } = useLoaderData()
   return (
     <p className='text-lg font-bold tracking-tighter text-black dark:text-white lg:text-2xl'>
