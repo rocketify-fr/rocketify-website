7d6a794cbfb67793b70e0d10853df1bcb188f2ab
Louis Cathala <delwiv@protonmail.com>
2024-05-09 16:35:57 +0200

    feat: manage top-level 404

---
+++ app/components/NotFound.tsx
@@ -0,0 +1,9 @@
+import Container from '~/components/Container'
+
+export default function () {
+  return (
+    <Container className='flex grow items-center justify-center'>
+      <div className='font-bai text-5xl'>Not found</div>
+    </Container>
+  )
+}
