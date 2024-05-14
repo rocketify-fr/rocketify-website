a8c8397b5e7e710df1455c5db9724412fc920138
Louis Cathala <delwiv@protonmail.com>
2024-05-14 13:00:49 +0200

    feat: mobile footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,6 @@
 import { PortableText } from '@portabletext/react'
 import clsx from 'clsx'
 
-import { Logo } from '~/components/Logo'
-
 import Container from './Container'
 import Headband from './content/Headband'
 import { Link } from './Link'
@@ -26,20 +24,21 @@ export function Footer({ data }) {
         to='/contact'
         title='Démarrer un projet'
       ></Headband>
-      <Container className='flex items-start justify-between pt-12'>
+      <Container className='flex flex-col items-start justify-between gap-8 pt-12 sm:flex-row'>
         <div className='flex flex-1 flex-col gap-4'>
           <img src={logo.url} alt={logo.alt} width={140} />
           <p className='text-md'>{description}</p>
         </div>
-        <div className='flex flex-1'>
-          <div className='flex flex-1 flex-col items-center'>
+        <div className='flex flex-1 flex-col gap-8 sm:flex-row'>
+          <div className='flex flex-1 flex-col sm:items-center'>
             <div className='flex flex-col gap-4'>
               <p className='font-bai text-xl'>{menuTitle}</p>
               {menu.map((link, i) => (
                 <Link
+                  key={i}
                   className={clsx(
                     i + 1 === menu.length &&
-                      'flex justify-center rounded-3xl border border-black bg-rGreen px-4',
+                      'flex justify-center self-start rounded-3xl border border-black bg-rGreen px-4 py-1',
                     'text-md'
                   )}
                   link={link}
@@ -49,10 +48,10 @@ export function Footer({ data }) {
               ))}
             </div>
           </div>
-          <div className='flex flex-1 flex-col items-end gap-4 text-md'>
+          <div className='flex flex-1 flex-col gap-4 text-md sm:items-end'>
             <p className='font-bai text-xl'>{contactTitle}</p>
             {contactMenu.map((text, i) => {
-              return <PortableText value={text}></PortableText>
+              return <PortableText key={i} value={text}></PortableText>
             })}
           </div>
         </div>
@@ -61,16 +60,19 @@ export function Footer({ data }) {
         <p className='py-2 font-bai text-xl'>{certificationsTitle}</p>
         <div className='grid grid-cols-1 justify-evenly gap-8 sm:grid-cols-2 lg:grid-cols-3'>
           {certifications.map((cert) => (
-            <div className='align-center flex flex-1 justify-between rounded-xl border border-black px-4 py-6'>
+            <div
+              key={cert.description}
+              className='flex flex-1 flex-col items-start justify-start rounded-xl border border-black p-4 sm:flex-row sm:items-center sm:justify-between sm:px-4 sm:py-6'
+            >
               <img
                 src={cert.logo.url}
                 width={64}
                 height={64}
-                className='size-[64px]'
+                className='size-[40px] sm:size-[64px]'
                 alt='Certification'
               />
-              <div className='flex flex-col items-end text-right'>
-                <p className='text-buttonLabelBody font-bai font-bold uppercase'>
+              <div className='flex flex-col gap-2 sm:items-end sm:text-right'>
+                <p className='font-bai text-paragraph font-bold uppercase'>
                   {cert.title}
                 </p>
                 <p className='font-work text-sm'>{cert.description}</p>
@@ -80,12 +82,19 @@ export function Footer({ data }) {
           ))}
         </div>
       </Container>
-      <Container className='mt-12 flex justify-between border-t border-t-black pb-12 pt-4'>
-        {menuSubFooter.menu.map((link) => (
-          <Link className='font-work text-xs underline' link={link}>
-            {link[link.linkType].title}
-          </Link>
-        ))}
+      <Container className='mt-12 flex flex-col gap-8 pb-12 pt-4'>
+        <div className='w-full border-t border-t-black'></div>
+        <div className='flex justify-between'>
+          {menuSubFooter.menu.map((link) => (
+            <Link
+              key={link[link.linkType].title}
+              className='font-work text-xs underline'
+              link={link}
+            >
+              {link[link.linkType].title}
+            </Link>
+          ))}
+        </div>
       </Container>
     </footer>
   )
