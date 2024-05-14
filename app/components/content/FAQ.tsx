cb8126fbafcf2e2c32f62024b3d5b932385eacce
Louis Cathala <delwiv@protonmail.com>
2024-05-14 16:12:09 +0200

    fix: faq Qs font size

--- app/components/content/FAQ.tsx
+++ app/components/content/FAQ.tsx
@@ -4,36 +4,25 @@ import Container from '../Container'
 import Separator from '../Separator'
 
 const FAQ = ({ title, description, faqItems }) => (
-  <Container className="flex flex-col">
+  <Container className='flex flex-col'>
     <h2 className='text-[56px]'>{title}</h2>
-    <p className='text-paragraph' >{description}</p>
+    <p className='text-paragraph'>{description}</p>
 
     <Separator></Separator>
     <div className='flex  flex-col'>
       {faqItems.map((item, i) => (
         <>
-          <div className="flex">
-            <h4 className='text-3xl flex-[2]'>{item.title}</h4>
-            <p className="text-paragraph flex-[3]">{item.description}</p>
+          <div className='flex'>
+            <h4 className='flex-[2] text-[26px]'>{item.title}</h4>
+            <p className='flex-[3] text-paragraph sm:pl-12'>
+              {item.description}
+            </p>
           </div>
-          {i + 1 < faqItems.length && (
-            <Separator></Separator>
-          )}
+          {i + 1 < faqItems.length && <Separator></Separator>}
         </>
-
       ))}
     </div>
   </Container>
 )
 
 export default FAQ
-
-
-
-
-
-
-
-
-
-
