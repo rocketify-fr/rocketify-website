76b927cb7d48da1220f5f8932b8221805ee8838c
Louis Cathala <delwiv@protonmail.com>
2024-05-15 14:00:10 +0200

    feat: add name to default export

--- app/components/NotFound.tsx
+++ app/components/NotFound.tsx
@@ -1,6 +1,6 @@
 import Container from '~/components/Container'
 
-export default function () {
+export default function NotFound() {
   return (
     <Container className='flex grow items-center justify-center'>
       <div className='font-bai text-5xl'>Not found</div>
