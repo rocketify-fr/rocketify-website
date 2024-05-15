1616ad2d4113148bd3f926bacb09dee3d960f732
Louis Cathala <delwiv@protonmail.com>
2024-05-15 13:49:35 +0200

    feat: generic route template with not found managed

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,9 +1,64 @@
-import Container from '~/components/Container'
+import type {LoaderFunctionArgs} from '@remix-run/node'
+import {json} from '@remix-run/node'
+import {useLoaderData} from '@remix-run/react'
+import {useQuery} from '@sanity/react-loader'
 
-export default function NotFound() {
+import {Loading} from '~/components/Loading'
+import NotFound from '~/components/NotFound'
+import PageComponent from '~/components/Page'
+import {loadQuery} from '~/sanity/loader.server'
+import {loadQueryOptions} from '~/sanity/loadQueryOptions.server'
+import {HOMEPAGE_QUERY, PAGE_QUERY, } from '~/sanity/queries'
+import type {RecordStub} from '~/types/record'
+
+export const loader = async ({request}: LoaderFunctionArgs) => {
+  const {options} = await loadQueryOptions(request.headers)
+  const slug = new URL(request.url).pathname.slice(1)
+  const query = PAGE_QUERY
+  const queryParams = { slug }
+
+  const initial = await loadQuery(
+    query,
+    queryParams,
+    options,
+  )
+
+  if (!initial.data) {
+    return {
+       notFound: true,
+    }
+  }
+
+  return json({
+    initial,
+    query,
+    params: queryParams,
+  })
+}
+
+export default function Index() {
+  const {notFound, initial, query, params} = useLoaderData<typeof loader>()
+  const {data, loading, encodeDataAttribute} = useQuery<typeof initial.data>(
+    query,
+    params,
+    {
+      // There's a TS issue with how initial comes over the wire
+      // @ts-expect-error
+      initial,
+    },
+  )
+
+  if (loading && !data) {
+    return <Loading />
+  } else if (!data || !initial.data || notFound) {
+    return <NotFound />
+  }
+
+  const pageData = data?.[0] || initial.data[0]
+
   return (
-    <Container className='flex grow items-center justify-center'>
-      <div className='font-bai text-5xl'>Not found</div>
-    </Container>
+    <PageComponent
+      {...pageData}
+    />
   )
 }
