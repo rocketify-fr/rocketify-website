8707059455073df9b90ce4b5898a3e64cd435153
Louis Cathala <delwiv@protonmail.com>
2024-05-16 11:59:23 +0200

    feat: add animation for service cards hover

--- app/components/content/ServiceHighlights.tsx
+++ app/components/content/ServiceHighlights.tsx
@@ -1,3 +1,7 @@
+import clsx from 'clsx'
+
+import { getRandomColor } from '~/utils/colors'
+
 import Container from '../Container'
 import ResponsiveGrid from '../layout/ResponsiveGrid'
 import { Link } from '../Link'
@@ -17,7 +21,7 @@ const ServiceHighlights = ({ title, description, services }) => {
               <Link
                 link={link}
                 key={i}
-                className='flex max-w-[350px] flex-col gap-4 overflow-hidden rounded-3xl border border-black bg-white p-6'
+                className='relative flex max-w-[350px] flex-col gap-4 overflow-hidden rounded-3xl border border-black bg-white p-6'
               >
                 <img src={icon.url} alt={icon.url} height={40} width={40} />
                 <h3 className='text-[26px]'>{title}</h3>
@@ -31,15 +35,22 @@ const ServiceHighlights = ({ title, description, services }) => {
         <ResponsiveGrid className='hidden sm:grid' gaps={8}>
           {services.map(({ title, description, icon, link }, i) => {
             return (
-              <Link
-                link={link}
-                key={i}
-                className='flex flex-col gap-4 rounded-3xl border border-black p-6'
-              >
-                <img src={icon.url} alt={icon.url} height={40} width={40} />
-                <h3 className='text-[26px]'>{title}</h3>
-                <p className='text-paragraph'>{description}</p>
-              </Link>
+              <div className='relative' key={i}>
+                <Link
+                  link={link}
+                  className='relative z-10 flex size-full flex-col gap-4 rounded-3xl border border-black bg-white p-6 transition-transform duration-500 hover:-translate-y-2 hover:translate-x-2'
+                >
+                  <img src={icon.url} alt={icon.url} height={40} width={40} />
+                  <h3 className='text-[26px]'>{title}</h3>
+                  <p className='text-paragraph'>{description}</p>
+                </Link>
+                <div
+                  className={clsx(
+                    'absolute left-0 top-0 z-0 size-full rounded-3xl',
+                    `bg-${getRandomColor()}`
+                  )}
+                ></div>
+              </div>
             )
           })}
         </ResponsiveGrid>
--- app/utils/colors.ts
+++ app/utils/colors.ts
@@ -15,3 +15,10 @@ export const colorNames = Object.keys(colors)
 export const getColor = (rawColor: String = 'black') => {
   return colorNames.find((color) => rawColor.includes(color))
 }
+
+export const getRandomColor = () => {
+  const colors = colorNames.filter(
+    (color) => !['black', 'rAzure', 'rDeepBlue', 'rDark'].includes(color)
+  )
+  return colors[Math.floor(Math.random() * colors.length)]
+}
