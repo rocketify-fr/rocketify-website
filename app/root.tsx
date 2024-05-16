37eb8ee5528e6b305e46a8794dcaa8446e018620
Louis Cathala <delwiv@protonmail.com>
2024-05-16 10:47:10 +0200

    feat: update favicon and text align

--- app/root.tsx
+++ app/root.tsx
@@ -118,7 +118,7 @@ export default function App() {
         <Meta />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <Links />
       </head>
       <body className={bodyClassNames}>
