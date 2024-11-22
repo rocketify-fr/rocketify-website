27b34083c86e24c699512ec9eb015affd67d9303
Louis Cathala <delwiv@protonmail.com>
2024-11-22 11:23:04 +0100

    feat: blog + blog post better language

--- app/components/Share.tsx
+++ app/components/Share.tsx
@@ -2,7 +2,11 @@ export const Share = ({ url }) => {
   return (
     <div className='flex gap-x-2'>
       {['link', 'linkedin', 'x', 'facebook'].map((name) => (
-        <div className='flex size-[64px] items-center justify-center rounded-full bg-gray-100 font-bai text-3xl font-bold'>
+        <div
+          key={name}
+          title={name}
+          className='flex size-[64px] items-center justify-center rounded-full bg-gray-100 font-bai text-3xl font-bold'
+        >
           {name[0]}
         </div>
       ))}
--- app/routes/($lang).blog.$slug.tsx
+++ app/routes/($lang).blog.$slug.tsx
@@ -1,31 +1,29 @@
-import type {LoaderFunctionArgs, MetaFunction} from '@remix-run/node'
-import {useLoaderData} from '@remix-run/react'
-import {useQuery} from '@sanity/react-loader'
-import BlogPost from '~/components/blog/BlogPost'
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { useQuery } from '@sanity/react-loader'
 
-import {Loading} from '~/components/Loading'
-import {loadQuery} from '~/sanity/loader.server'
-import {loadQueryOptions} from '~/sanity/loadQueryOptions.server'
-import {POST_QUERY} from '~/sanity/queries'
+import BlogPost from '~/components/blog/BlogPost'
+import { Loading } from '~/components/Loading'
+import { loadQuery } from '~/sanity/loader.server'
+import { loadQueryOptions } from '~/sanity/loadQueryOptions.server'
+import { POST_QUERY } from '~/sanity/queries'
 import { getLanguage } from '~/utils/language'
 
 // Load the `record` document with this slug
-export const loader = async ({params, request}: LoaderFunctionArgs) => {
-  const lang = getLanguage(params)
-  const {options} = await loadQueryOptions(request.headers)
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const language = getLanguage(params)
+  const { options } = await loadQueryOptions(request.headers)
 
   const query = POST_QUERY
-  const initial = await loadQuery(
-    query,
-    {...params, lang},
-    options,
-  ).then((res) => ({...res, data: res.data ? res.data : null}))
+  const initial = await loadQuery(query, { ...params, language }, options).then(
+    (res) => ({ ...res, data: res.data ? res.data : null })
+  )
   if (!initial.data) {
-    throw new Response('Not found', {status: 404})
+    throw new Response('Not found', { status: 404 })
   }
 
   // Create social share image url
-  const {origin} = new URL(request.url)
+  const { origin } = new URL(request.url)
   const ogImageUrl = `${origin}/resource/og?id=${initial.data._id}`
 
   return {
@@ -40,27 +38,29 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
   const metaTags = [
     { title: data.initial?.data?.seo?.title },
     { name: 'description', content: data.initial?.data?.seo?.description },
-  ];
-  
-  const publishStatus = data?.initial?.data?.publishStatus?.replace(/[^\x20-\x7E]/g, '').trim();
-  
-  if (publishStatus === "hidden") {
-    metaTags.push({ name: 'robots', content: 'noindex' });
+  ]
+
+  const publishStatus = data?.initial?.data?.publishStatus
+    ?.replace(/[^\x20-\x7E]/g, '')
+    .trim()
+
+  if (publishStatus === 'hidden') {
+    metaTags.push({ name: 'robots', content: 'noindex' })
   }
 
-  return metaTags;
-};
+  return metaTags
+}
 
 export default function PostPage() {
-  const {initial, query, params} = useLoaderData<typeof loader>()
-  const {data, loading, encodeDataAttribute} = useQuery<typeof initial.data>(
+  const { initial, query, params } = useLoaderData<typeof loader>()
+  const { data, loading, encodeDataAttribute } = useQuery<typeof initial.data>(
     query,
     params,
     {
       // There's a TS issue with how initial comes over the wire
       // @ts-expect-error
       initial,
-    },
+    }
   )
 
   if (loading && !data) {
@@ -69,9 +69,5 @@ export default function PostPage() {
     return <div>Not found</div>
   }
 
-  return (
-    <BlogPost
-      post={data || initial.data}
-    />
-  )
+  return <BlogPost post={data || initial.data} />
 }
