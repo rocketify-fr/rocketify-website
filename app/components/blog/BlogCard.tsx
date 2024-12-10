fb62b81802045ef8081eb0d0112542afb1c8131e
Louis Cathala <delwiv@protonmail.com>
2024-12-10 12:12:09 +0100

    feat: better cards

--- app/components/blog/BlogCard.tsx
+++ app/components/blog/BlogCard.tsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData } from '@remix-run/react'
 import clsx from 'clsx'
 
+import { getRandomColor } from '~/utils/colors'
 import { getLocalizedPath } from '~/utils/language'
 
 import { PreHeader } from '../post/BlogHeader'
@@ -11,48 +12,57 @@ export default function PostCard({
   className = null,
 }) {
   return (
-    <Link
-      to={`${getLocalizedPath(post.language, `/blog/${post.slug}`)}`}
-      className={clsx(
-        'flex overflow-hidden rounded-3xl border border-black',
-        !horizontal && 'flex-col',
-        className
-      )}
-    >
-      <img
-        src={post.image.url}
-        alt={post.image.alt}
+    <div className='relative'>
+      <Link
+        to={`${getLocalizedPath(post.language, `/blog/${post.slug}`)}`}
         className={clsx(
-          'w-full border-b border-black object-cover',
-          horizontal ? 'lg:aspect-[11/10] lg:flex-[2]' : 'aspect-video'
-        )}
-      />
-      <div
-        className={clsx(
-          'flex flex-col p-4',
-          horizontal && 'lg:my-auto lg:flex-[1] lg:px-6'
+          'z-10 flex overflow-hidden rounded-3xl border border-black bg-white',
+          'transition-transform duration-500 hover:-translate-y-2 hover:translate-x-2',
+          horizontal ? 'flex-col lg:flex-row' : 'flex-col',
+          className
         )}
       >
-        <PreHeader
-          tags={post.tags}
-          estimatedReadingTime={post.estimatedReadingTime}
-        ></PreHeader>
-        <h3 className='font-bai text-[26px] leading-[100%]'>{post.title}</h3>
-        <p className='text-sm'>{post.description}</p>
-        <div className='flex items-center justify-between pt-4'>
-          <div className='flex items-center gap-x-4'>
-            <img
-              width={40}
-              height={40}
-              src={post.author.image.url}
-              alt={post.author.image.alt}
-              className='size-[40px] rounded-full'
-            />
-            <p>{post.author.name}</p>
+        <img
+          src={post.image.url}
+          alt={post.image.alt}
+          className={clsx(
+            'w-full object-cover',
+            horizontal ? 'lg:aspect-video lg:flex-[2]' : 'aspect-video'
+          )}
+        />
+        <div
+          className={clsx(
+            'flex flex-col p-4',
+            horizontal && 'lg:my-auto lg:flex-[1] lg:px-6'
+          )}
+        >
+          <PreHeader
+            tags={post.tags}
+            estimatedReadingTime={post.estimatedReadingTime}
+          ></PreHeader>
+          <h3 className='font-bai text-[26px] leading-[100%]'>{post.title}</h3>
+          <p className='line-clamp-3 text-sm'>{post.description}</p>
+          <div className='flex items-center justify-between pt-4'>
+            <div className='flex items-center gap-x-4'>
+              <img
+                width={40}
+                height={40}
+                src={post.author.image.url}
+                alt={post.author.image.alt}
+                className='size-[40px] rounded-full'
+              />
+              <p>{post.author.name}</p>
+            </div>
+            <div>{new Date(post._createdAt).toLocaleDateString()}</div>
           </div>
-          <div>{new Date(post._createdAt).toLocaleDateString()}</div>
         </div>
-      </div>
-    </Link>
+      </Link>
+      <div
+        className={clsx(
+          'absolute left-0 top-0 -z-10 size-full rounded-3xl',
+          `bg-${getRandomColor()}`
+        )}
+      ></div>
+    </div>
   )
 }
