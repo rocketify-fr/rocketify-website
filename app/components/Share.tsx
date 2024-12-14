f3dff55aa4c9ab75a7ce858a2c16f67b93dbc2c9
Louis Cathala <delwiv@protonmail.com>
2024-12-14 17:37:19 +0100

    feat: add social icons

--- app/components/Share.tsx
+++ app/components/Share.tsx
@@ -1,13 +1,13 @@
 export const Share = ({ url }) => {
   return (
     <div className='flex gap-x-2'>
-      {['link', 'linkedin', 'x', 'facebook'].map((name) => (
+      {['link', 'linkedin', 'twitter', 'facebook'].map((name) => (
         <div
           key={name}
           title={name}
-          className='flex size-[64px] items-center justify-center rounded-full bg-gray-100 font-bai text-3xl font-bold'
+          className='flex size-[32px] cursor-pointer items-center justify-center rounded-full bg-gray-100 font-bai text-3xl font-bold'
         >
-          {name[0]}
+          <img src={`/img/share-${name}.svg`} alt='' />
         </div>
       ))}
     </div>
