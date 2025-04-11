3920ce2e29e5561e1deecc165a165eb766e89ae9
Louis Cathala <delwiv@protonmail.com>
2025-04-11 21:16:36 +0200

    dx: add remix dev tools, but disable for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import { vercelPreset } from '@vercel/remix/vite'
+// import { remixDevTools } from 'remix-development-tools'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -11,6 +12,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
+    // remixDevTools(),
     remix({
       presets: [vercelPreset()],
     }),
