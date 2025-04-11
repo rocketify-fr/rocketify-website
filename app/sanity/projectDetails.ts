c957ef83d40eacec4c0f9687d2869f925088c4af
Louis Cathala <delwiv@protonmail.com>
2025-04-11 15:40:25 +0200

    feat: use dev dataset

--- app/sanity/projectDetails.ts
+++ app/sanity/projectDetails.ts
@@ -1,5 +1,5 @@
 export const projectId = 'd55vxv0n'
-export const dataset = 'production'
+export const dataset = 'dev'
 export const apiVersion = '2024-02-13'
 
 export const projectDetails = () => ({
